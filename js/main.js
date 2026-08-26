8384a4d94fb2304bcdac38609ec75a7c0fa19ea3
Samarssj <126043595+Samarssj@users.noreply.github.com>
2026-08-26 22:01:09 +0000

    Fill spiral gaps with denser stars

--- js/main.js
+++ js/main.js
@@ -247,7 +247,7 @@ function createMilkyWay() {
     colors[index + 2] = color.b;
   }
 
-  const backgroundCount = 60000;
+  const backgroundCount = 100000;
   const backgroundPositions = new Float32Array(backgroundCount * 3);
   const backgroundColors = new Float32Array(backgroundCount * 3);
   for (let i = 0; i < backgroundCount; i += 1) {
